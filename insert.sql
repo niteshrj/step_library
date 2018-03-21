1070e533cc23d963d17369022edad9b2b2616506
Nitesh Ranjan <nitesh.ranja@gmail.com>
2018-03-21 11:16:01 +0530

    removed distinct from first query

--- insert.sql
+++ insert.sql
@@ -1,6 +1,6 @@
 SET search_path TO library;
 
---run this script
+--run this script for heroku
 --heroku pg:psql --app dbone < /Users/niteshr/projects/step_library/insert.sql
 --
 \set pwd '\'':p'/book_detail.txt\''
